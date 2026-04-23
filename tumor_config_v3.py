39127fb4cfcfc8aa90a9915b913f41160c662e04
moradbelmelih <morad.bel-melih@insa-lyon.fr>
2026-04-23 17:18:08 -0400

    fix les paths de config

--- tumor_config_v3.py
+++ tumor_config_v3.py
@@ -3,31 +3,33 @@ _base_ = 'grounding_dino_swin-t_pretrain_obj365_goldg.py'
 # 1. Nouveaux chemins et métadonnées (V3)
 data_root = 'data/MSD_pancreas/'
 
-# Changement sémantique pour forcer DINO à regarder le tissu environnant
-metainfo = dict(classes=('tumor',), palette=[(255, 0, 0)])
-# N'oublie pas de créer ce nouveau fichier JSON !
+# Deux classes pour matcher le checkpoint finetuné (epoch_25)
+metainfo = dict(
+    classes=('pancreas', 'tumor'),
+    palette=[(0, 255, 0), (255, 0, 0)],
+)
 label_map_path = 'data/MSD_pancreas/tumor_label_map.json'
 
 # 2. Architecture
 model = dict(
-    backbone=dict(frozen_stages=2)
+    backbone=dict(frozen_stages=0)
 )
 
-# 3. Pipelines avec distorsion photométrique
+# 3. Pipelines (scales alignées avec l'entraînement du checkpoint : 512x512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', scale=(800, 1333), keep_ratio=True),
-    dict(type='PhotoMetricDistortion', 
-         brightness_delta=32, 
-         contrast_range=(0.5, 1.5), 
-         saturation_range=(0.5, 1.5), 
-         hue_delta=18),
+    dict(type='Resize', scale=(512, 512), keep_ratio=True),
     dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion',
+         brightness_delta=32,
+         contrast_range=(0.5, 1.5),
+         saturation_range=(0.5, 1.5),
+         hue_delta=18),
     dict(
         type='RandomSamplingNegPos',
         tokenizer_name='bert-base-uncased',
-        num_sample_negative=0,
+        num_sample_negative=3,
         label_map_file=label_map_path),
     dict(
         type='PackDetInputs',
@@ -38,7 +40,7 @@ train_pipeline = [
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=(800, 1333), keep_ratio=True),
+    dict(type='Resize', scale=(512, 512), keep_ratio=True),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='PackDetInputs',
@@ -46,10 +48,10 @@ test_pipeline = [
                    'scale_factor', 'text', 'custom_entities'))
 ]
 
-# 4. Dataloaders - LE CORRECTIF ANTI-CRASH EST ICI
+# 4. Dataloaders
 train_dataloader = dict(
-    batch_size=1, 
-    num_workers=0, # <-- 0 FORCE LE CHARGEMENT LINÉAIRE. Finis les conflits CPU/GPU.
+    batch_size=1,
+    num_workers=0,
     persistent_workers=False,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
@@ -66,7 +68,7 @@ train_dataloader = dict(
 
 val_dataloader = dict(
     batch_size=1,
-    num_workers=0, # <-- Même chose ici
+    num_workers=0,
     persistent_workers=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
@@ -82,14 +84,39 @@ val_dataloader = dict(
     )
 )
 
+# Override test_dataloader (sinon _base_ pointe vers COCO 2017 avec scale 800x1333)
+# C'est celui-ci que mmdet.inference_detector lit pour construire son pipeline.
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        _delete_=True,
+        type='CocoDataset',
+        data_root=data_root,
+        metainfo=metainfo,
+        return_classes=True,
+        ann_file='test.json',
+        data_prefix=dict(img=''),
+        test_mode=True,
+        pipeline=test_pipeline
+    )
+)
+
 val_evaluator = dict(
     type='CocoMetric',
     ann_file=data_root + 'val.json',
     metric='bbox'
 )
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'test.json',
+    metric='bbox'
+)
 
 # 5. Paramètres d'entraînement
-fp16 = dict(loss_scale='dynamic') # Maintien de l'économie VRAM
+fp16 = dict(loss_scale='dynamic')
 
 optim_wrapper = dict(
     type='OptimWrapper',
@@ -102,9 +129,9 @@ train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=25, val_interval=1)
 
 default_hooks = dict(
     checkpoint=dict(
-        type='CheckpointHook', 
-        interval=1, 
-        max_keep_ckpts=1, 
+        type='CheckpointHook',
+        interval=1,
+        max_keep_ckpts=1,
         save_optimizer=False,
         save_best='coco/bbox_mAP'
     ),
